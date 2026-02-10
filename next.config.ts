f38983223cf48ba813644f06aba53f82bb305d5c
GKRBROS <gokul.kiran03@gmail.com>
2026-02-10 22:29:35 +0530

    refactor: migrate OTP logic to backend and cleanup frontend routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/otp/:path*",
+        destination: "https://scaleup.frameforge.one/scaleup2026/otp/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
